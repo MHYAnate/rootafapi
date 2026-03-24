968f7bf868dc0638c442e9e61c6bb9d5e7ccdb0e
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2026-03-24 14:48:31 +0100

    feat:newCheck

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -2,5 +2,10 @@
 import { Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
-@Module({ controllers: [EventsController], providers: [EventsService] })
+
+@Module({
+  controllers: [EventsController],
+  providers: [EventsService],
+  exports: [EventsService],
+})
 export class EventsModule {}
